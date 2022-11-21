bd25b69f85f036e3352a1a525f0dbdbfe9938339
muksiturrahman <93493026+muksiturrahman@users.noreply.github.com>
2022-11-21 17:25:10 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FirstProject());
+  runApp(MyProfile());
 }
-class FirstProject extends StatelessWidget {
-  const FirstProject({Key? key}) : super(key: key);
+class MyProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Text("First Project of Flutter"),
       theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Profile'),
+        ),
+          body: Container(
+            decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.all(
+                    Radius.circular(30)
+
+                ),
+              border: Border.all(
+                color: Colors.green,
+                width: 10,
+              )
+            ),
+            padding: EdgeInsets.only(left: 20, right: 20,top: 10, bottom: 10 ),
+            margin: EdgeInsets.all(20),
+            height: 100,
+            width: 100,
+            child: Text('First Application'),
+            //child: Center(child: Text('First Application')),
+        ),
+
+    ),
+      ),
     );
   }
 }
+
